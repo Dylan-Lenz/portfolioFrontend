86760872b6fda25ba8c7f91c22ffc8c1302ed104
Dylan Lenz <dylanjlenz@gmail.com>
2023-03-21 09:04:45 -0500

    Added HeadShot

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import BlackV from "../../assets/BlackV.jpg";
 
 const NavBar = () => {
 
     return ( 
         <header className="navBar">
-            <ul>
-                <li className="navBar__links">
-                    <Link to='/'>
-                        <button>Welcome</button>
-                    </Link>
-                    <Link to='/projects'>
-                        <button>Projects</button>
-                    </Link>
-                </li>
-                <li>
-                    <h2 className="navBar__header">Dylan Lenz</h2>
-                </li>
-                <li>
-                    <p className="navBar__motto">
-                        “I have failed over and over again in my life,
-                        and that is why I succeed.” —Michael Jordan
-                    </p>
-                </li>
-            </ul>
+            <img src={BlackV} alt="headshot" />
+                <ul>
+                    <li>
+                        <h2 className="navBar__header">Dylan Lenz</h2>
+                    </li>
+                    <li className="navBar__links">
+                        <Link to='/'>
+                            <button>Welcome</button>
+                        </Link>
+                        <Link to='/projects'>
+                            <button>Projects</button>
+                        </Link>
+                    </li>
+                </ul>
         </header>
     );
 }
