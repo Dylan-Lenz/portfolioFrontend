30c659f460cab13df3b33220e742da048f9766bd
Dylan Lenz <dylanjlenz@gmail.com>
2023-03-21 10:03:33 -0500

    Added Icons

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+
+// Asset Imports
 import BlackV from "../../assets/BlackV.jpg";
+import LI from "../../assets/LI.png";
+import github from "../../assets/github.png"
 
 const NavBar = () => {
 
     return ( 
         <div className="navBar__cont">
-            <img src={BlackV} alt="headshot" />
-            <h2 className="navBar__header">Dylan Lenz</h2>
+            <div className="navBar__headshot">
+                <img src={BlackV} alt="headshot" />
+                <ul>
+                    <li>
+                        <a href="https://www.linkedin.com/in/dylan-lenz-92b27823b/"><img src={LI} alt="LI__icon" /></a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/Dylan-Lenz"><img src={github} alt="github__icon" /></a>
+                    </li>
+                </ul>
+            </div>
+            <div className="navBar__basics">
+                <h2 className="navBar__header">Dylan Lenz</h2>
                 <ul className="navBar__skills">
                     <li>
                         <p>FullStack</p>
@@ -25,14 +40,32 @@ const NavBar = () => {
                         <p>HTML/CSS</p>
                     </li>
                 </ul>
-                <Link to='/'>
-                    <button>Welcome</button>
-                </Link>
-                <Link to='/projects'>
-                    <button>Projects</button>
-                </Link>
+            </div>
+            <div className="navBar__location">
+                <ul>
+                    <li>
+                        <p>Madison, Wi, US</p>
+                        <p>Office Preference: Hybrid - 100% Remote - Office - Willing to Relocate</p>
+                    </li>
+                </ul>
+            </div>
+            <div className="navBar__buttons">
+                <ul >
+                    <li>
+                        <Link to='/'>
+                            <button>Welcome</button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/projects'>
+                            <button>Projects</button>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
  
 export default NavBar;
+
