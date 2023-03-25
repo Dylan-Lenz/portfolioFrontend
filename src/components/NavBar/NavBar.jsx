6aae83d4bc866422ac4150b370c52b35e2a58611
Dylan Lenz <dylanjlenz@gmail.com>
2023-03-25 09:41:41 -0500

    Styled NavBar Links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,11 +5,11 @@ import "../NavBar/NavBar.css"
 const NavBar = () => {
     return ( 
         <div className="container__navBar">
-            <a href='#about'><button>About</button></a>
-            <a href='#certification'><button>Certification</button></a>
+            <a href='#about'><h2 className="links__navBar">About</h2></a>
+            <a href='#certification'><h2 className="links__navBar">Certification</h2></a>
             <a href="/"><h1 className='header__navBar'>DYLAN LENZ</h1></a>
-            <a href='#work'><button>The Work</button></a>
-            <a href='#contact'><button>Contact</button></a>
+            <a href='#work'><h2 className="links__navBar">TheWork</h2></a>
+            <a href='#contact'><h2 className="links__navBar">Contact</h2></a>
         </div>
     );
 }
