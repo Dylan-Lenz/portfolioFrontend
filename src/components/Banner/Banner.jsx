b83a1c980cd8b72476cfc5664929756751eb7309
Dylan Lenz <dylanjlenz@gmail.com>
2023-04-05 14:15:44 -0500

    Updated Picture

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import "./Banner.css";
 
 // Asset Imports
-import BlackV from "../../assets/BlackV.jpg";
+import headshot from "../../assets/BlackV.jpg";
 import simpleflourish from "../../assets/simpleflourish.png"
 
 const InformationSection = () => {
     return(
         <div className="container__information">
             <img className='flourish' src={simpleflourish} alt="headshot" />
-            <img className='headshot' src={BlackV} alt="headshot" />
+            <img className='headshot' src={headshot} alt="headshot" />
             <img className='flourish' src={simpleflourish} alt="headshot" />
         </div>
     );
